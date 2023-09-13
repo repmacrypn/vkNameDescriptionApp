83d5dc4b5bc03e2cdfd4442edde574f7ec58861a
repmacrypn <artik3267@gmail.com>
2023-09-13 20:02:43 +0300

    fix: vite upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vkNameDescription/',
+  base: '/vkNameDescriptionApp/',
   plugins: [react()],
   resolve: {
     alias: {
